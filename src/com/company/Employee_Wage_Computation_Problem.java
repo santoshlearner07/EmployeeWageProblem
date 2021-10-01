764c6f2e3f0741bcb0772454a17535b88b7ae4ca
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-01 22:19:04 +0530

    Part time Income and Hour added

--- src/com/company/Employee_Wage_Computation_Problem.java
+++ src/com/company/Employee_Wage_Computation_Problem.java
@@ -3,6 +3,7 @@ package com.company;
 public class Employee_Wage_Computation_Problem {
     //constants
     public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int PER_DAY_WORK_HOUR = 8;
 
@@ -11,12 +12,19 @@ public class Employee_Wage_Computation_Problem {
         int empPerDayIncome = 0;
         System.out.println("Welcome to Employee Wage Computation Problem");
 
-        double empCheck = Math.floor(Math.random() * 10 % 2);
+        double empCheck = Math.floor(Math.random() * 10 % 3);
         if (empCheck == IS_FULL_TIME) {
             System.out.println("Employee is Present");
             empPerDayIncome = EMP_RATE_PER_HOUR * PER_DAY_WORK_HOUR;
             System.out.println("Employee per day Income :- " + empPerDayIncome + " rs");
+
+        } else if (empCheck == IS_PART_TIME)
+        {
+            System.out.println("Employee worked half day");
+            empPerDayIncome = (EMP_RATE_PER_HOUR * PER_DAY_WORK_HOUR)/2;
+            System.out.println("Employee half day Income :- " + empPerDayIncome + " rs");
         } else
+            
             System.out.println("Employee is Absent");
     }
 }
