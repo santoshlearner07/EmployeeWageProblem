f62e1623d8e74ad89d947129c0df46d28ea85286
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-01 21:32:52 +0530

    Welcome to Employee

---
+++ src/com/company/Employee_Wage_Computation_Problem.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Employee_Wage_Computation_Problem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Problem");
+    }
+}
