8de1e7525992d9eb23d5f27ad487f57915c02a34
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-01 23:27:18 +0530

    Wage Calculated for 20 days using Switch case

--- src/com/company/Employee_Wage_Computation_Problem.java
+++ src/com/company/Employee_Wage_Computation_Problem.java
@@ -6,38 +6,37 @@ public class Employee_Wage_Computation_Problem {
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int PER_DAY_WORK_HOUR = 8;
-
+    public static final int NUM_OF_WORKING_DAYS = 20;
 
     public static void main(String[] args) {
         int empPerDayIncome = 0;
-        int empHrs = 0;
-        System.out.println("Welcome to Employee Wage Computation Problem");
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+        int totalIncome = 0;
+        while (totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
+            int empHrs = 0;
 
-        int empCheck = (int) Math.floor(Math.random() * 10 % 3);
-//        if (empCheck == IS_FULL_TIME) {
-//            System.out.println("Employee is Present");
-//            empPerDayIncome = EMP_RATE_PER_HOUR * PER_DAY_WORK_HOUR;
-//            System.out.println("Employee per day Income :- " + empPerDayIncome + " rs");
-//
-//        } else if (empCheck == IS_PART_TIME)
-//        {
-//            System.out.println("Employee worked half day");
-//            empPerDayIncome = (EMP_RATE_PER_HOUR * PER_DAY_WORK_HOUR)/2;
-//            System.out.println("Employee half day Income :- " + empPerDayIncome + " rs");
-//        } else
-//
-//            System.out.println("Employee is Absent");
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
+            int empCheck = ((int) Math.floor(Math.random() * 10) % 3);
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    empPerDayIncome = PER_DAY_WORK_HOUR * EMP_RATE_PER_HOUR;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    empPerDayIncome = (PER_DAY_WORK_HOUR * EMP_RATE_PER_HOUR) / 2;
+                    break;
+                default:
+                    empHrs = 0;
+
+            }
+            totalEmpHrs += empHrs;
+            totalIncome += empPerDayIncome;
+
+            int monthlyIncome = empHrs * totalWorkingDays;
+            System.out.println("Day " + totalWorkingDays + " earned " + totalIncome + " rs");
+
         }
-
-
     }
 }
