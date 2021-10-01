6fd57c5dda4d90882cde44b7ef5809e350eba854
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-01 22:33:30 +0530

    Switch case added to make it more readable

--- src/com/company/Employee_Wage_Computation_Problem.java
+++ src/com/company/Employee_Wage_Computation_Problem.java
@@ -10,21 +10,34 @@ public class Employee_Wage_Computation_Problem {
 
     public static void main(String[] args) {
         int empPerDayIncome = 0;
+        int empHrs = 0;
         System.out.println("Welcome to Employee Wage Computation Problem");
 
-        double empCheck = Math.floor(Math.random() * 10 % 3);
-        if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present");
-            empPerDayIncome = EMP_RATE_PER_HOUR * PER_DAY_WORK_HOUR;
-            System.out.println("Employee per day Income :- " + empPerDayIncome + " rs");
+        int empCheck = (int) Math.floor(Math.random() * 10 % 3);
+//        if (empCheck == IS_FULL_TIME) {
+//            System.out.println("Employee is Present");
+//            empPerDayIncome = EMP_RATE_PER_HOUR * PER_DAY_WORK_HOUR;
+//            System.out.println("Employee per day Income :- " + empPerDayIncome + " rs");
+//
+//        } else if (empCheck == IS_PART_TIME)
+//        {
+//            System.out.println("Employee worked half day");
+//            empPerDayIncome = (EMP_RATE_PER_HOUR * PER_DAY_WORK_HOUR)/2;
+//            System.out.println("Employee half day Income :- " + empPerDayIncome + " rs");
+//        } else
+//
+//            System.out.println("Employee is Absent");
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
+        }
 
-        } else if (empCheck == IS_PART_TIME)
-        {
-            System.out.println("Employee worked half day");
-            empPerDayIncome = (EMP_RATE_PER_HOUR * PER_DAY_WORK_HOUR)/2;
-            System.out.println("Employee half day Income :- " + empPerDayIncome + " rs");
-        } else
-            
-            System.out.println("Employee is Absent");
+
     }
 }
